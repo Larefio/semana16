443990b50b958b1aa4c8fbfcb0f1a1b6370fa7a5
luis <effio_1@hotmail.com>
2026-06-09 12:03:51 -0500

    añadimos hero y lo importamos en App.jsx

---
+++ bakery/src/components/hero.jsx
@@ -0,0 +1,14 @@
+import './Hero.css'
+
+const Hero = () => {
+  return (
+    <section className="hero" id="home">
+      <div className="hero__content">
+        <h1 className="hero__title">Let's Taste Perfection</h1>
+        <a href="#reservation" className="hero__btn">Order Now</a>
+      </div>
+    </section>
+  )
+}
+
+export default Hero
