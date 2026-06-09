abe8f0f3f73d58ca328b5b76c0dbbce20dd70981
luis <effio_1@hotmail.com>
2026-06-09 12:50:22 -0500

    la ultima parte de la pagina continuamos con el footer

---
+++ bakery/src/components/bakersDelight.jsx
@@ -0,0 +1,26 @@
+import './BakersDelight.css'
+
+const BakersDelight = () => {
+  return (
+    <section className="delight" id="reservation">
+      <div className="delight__container">
+        <div className="delight__text">
+          <span className="delight__script">Baker's</span>
+          <h2 className="delight__title">Delight</h2>
+          <p className="delight__body">
+            Lorem ipsum dolor sit amet consectetur adipiscing elit. Adipisci
+            voluptatum dolor earum accusamus suscipit. Officia atque vitae
+            excepturi deserunt nihil?
+          </p>
+          <a href="#reservation" className="delight__link">Make a Reservation</a>
+        </div>
+        <div className="delight__images">
+          <img src="/bakers-delight-1.jpg" alt="bakers delight" />
+          <img src="/bakers-delight-2.jpg" alt="bakers delight" />
+        </div>
+      </div>
+    </section>
+  )
+}
+
+export default BakersDelight
